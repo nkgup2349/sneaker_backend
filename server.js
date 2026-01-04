0d9394724bb6bd28f850e3dc745a59375a11a157
Nikhil Kumar Gupta <118952235+nkgup2349@users.noreply.github.com>
2026-01-04 22:14:51 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -14,4 +14,6 @@ app.use("/orderStatus", require("./routes/orderstate"));
 app.use("/reviews", require("./routes/reviews"));
 // app.use("/users", require("./routes/users"));
 
-app.listen(5000, () => console.log("Backend running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT , () => console.log(`Backend running on ${PORT}`));
